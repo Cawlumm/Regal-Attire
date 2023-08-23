7eb54849273a332538d6cadebb0cc3ac73e8aec6
Carter Lumm <cwlumm@gmail.com>
2023-08-23 16:22:01 -0400

    server bugs

---
+++ functions/stripe.js
@@ -0,0 +1,43 @@
+const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY);
+
+exports.handler = async (event) => {
+    if (event.httpMethod !== 'POST') {
+        return {
+            statusCode: 405,
+            body: JSON.stringify({ error: 'Method Not Allowed' }),
+        };
+    }
+
+    try {
+        const requestData = JSON.parse(event.body);
+
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ['card'],
+            mode: 'payment',
+            line_items: requestData.items.map(item => {
+                return {
+                    price_data: {
+                        currency: 'usd',
+                        product_data: {
+                            name: item.name,
+                        },
+                        unit_amount: Math.round(item.price * 100),
+                    },
+                    quantity: item.quantity,
+                };
+            }),
+            success_url: `${process.env.SERVER_URL}`,
+            cancel_url: `${process.env.SERVER_URL}`,
+        });
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ url: session.url }),
+        };
+    } catch (e) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: e.message }),
+        };
+    }
+};
