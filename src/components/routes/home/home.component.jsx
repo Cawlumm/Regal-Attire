5bae1b369580badc395e49011b6521e09ea88e63
Carter Lumm <cwlumm@gmail.com>
2023-09-07 11:45:15 -0400

    added declaration component and welcom-banner animation

--- src/components/routes/home/home.component.jsx
+++ src/components/routes/home/home.component.jsx
@@ -1,9 +1,11 @@
 import Directory from '../../directory/directory.component';
 import WelcomeBanner from '../../welcome-banner/welcome-banner.component';
+import Declaration from '../../declaration/declaration.component';
 import './home.styles.scss';
 const Home = () => {
   return (<div className='home-container'>
     <WelcomeBanner />
+    <Declaration/>
     <Directory />
   </div>)
 };
